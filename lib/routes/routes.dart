0c21dfb97d3f8c71681f682517f406de6424d0cf
RicardoFariaC <ricardofcosta0804@gmail.com>
2024-05-13 13:56:19 -0300

    feat(routes): add routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:stock_app/pages/home.dart';
+import 'package:stock_app/pages/views/identify_view.dart';
+import 'package:stock_app/pages/views/login_view.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
-  "/": (context) => Home(),
+  "/": (context) => LoginView(),
+  "/home": (context) => Home(),
+  "/camera": (context) => const IdentifyView(),
 };
